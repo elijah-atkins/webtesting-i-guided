88f82467a6606425a55e861127add53ce96b554e
Sean Kirkby <skrkby@kirkby.org>
2020-08-11 09:48:25 -0600

    Try not. Do, or do not. There is no "try".

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,24 +2,15 @@ module.exports = {
   add,
 };
 
-// function add() {
-//   let sum = 0;
-//   for (var i = 0; i < arguments.length; i++) {
-//     sum += arguments[i];
-//   }
-//   return sum;
-// }
-
-// function add(...values) {
-// 	return values.reduce((a, b) => a + b, 0)
-// }
-
 function add(args) {
+
   const values = Array.isArray(args) ? args : Array.from(arguments);
 
-  return values.reduce((acc, el) => { return acc + el }, 0)
-}
+  return values.reduce((sum, value) => {
+    if ((typeof (value) !== 'number') || Number.isNaN(value)) {
+      throw new Error('invalid args')
+    };
+    return sum + value;
+  }, 0);
 
-function staticString() {
-  return "this is a static"
 }
