8ed00a08b3da0e23dd4c6a9cc90ff710d4ccc322
Sean Kirkby <sean.kirkby@niceincontact.com>
2020-08-11 09:44:54 -0600

    interim

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -2,6 +2,24 @@ module.exports = {
   add,
 };
 
-function add() {
-  return null;
+// function add() {
+//   let sum = 0;
+//   for (var i = 0; i < arguments.length; i++) {
+//     sum += arguments[i];
+//   }
+//   return sum;
+// }
+
+// function add(...values) {
+// 	return values.reduce((a, b) => a + b, 0)
+// }
+
+function add(args) {
+  const values = Array.isArray(args) ? args : Array.from(arguments);
+
+  return values.reduce((acc, el) => { return acc + el }, 0)
+}
+
+function staticString() {
+  return "this is a static"
 }
